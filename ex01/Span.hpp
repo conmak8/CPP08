aa3dfec5f9d02ffb45c62ccc2052fd877f2c4af3
Constantinos Makariou <cmakario@student.42heilbronn.de>
2025-05-03 15:41:31 +0200

    01:start

---
+++ ex01/Span.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/03 15:33:11 by cmakario          #+#    #+#             */
+/*   Updated: 2025/05/03 15:34:57 by cmakario         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+# include <iostream>
+# include <vector>
+# include <algorithm>
+# include <exception>
+# include <limits>
+# include <iterator>
+# include <stdexcept>
+# include <cstdlib>
+# include <ctime>
+
+
+
+#endif // !SPAN_HPP
