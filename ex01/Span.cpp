6e4f91d3743065c1e4b7b7a49b168be070ee671f
cmakario <cmakario@student.42.fr>
2025-05-04 13:28:16 +0200

    01:basic implementation

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 15:53:16 by cmakario          #+#    #+#             */
-/*   Updated: 2025/05/03 16:09:04 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/05/04 13:26:52 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,11 @@ Span::Span() : _maxSize(0) {}
 Span::Span(unsigned int n) : _maxSize(n) {}
 
 Span::Span(const Span& src) : _maxSize(src._maxSize), _numbers(src._numbers) {}
-// Span::Span(const Span& src) = default; 
-// Span::Span(const Span& src)
-// {
-	// *this = src;
-// }
+// Span::Span(const Span& src) = default;
 
 Span& Span::operator=(const Span& rhs)
 {
-	if (this != &rhs) 
+	if (this != &rhs)
 	{
 		_maxSize = rhs._maxSize;
 		_numbers = rhs._numbers;
@@ -36,3 +32,43 @@ Span& Span::operator=(const Span& rhs)
 
 Span::~Span() {}
 // Span::~Span() = default;
+
+void Span::addNumber(int number)
+{
+	if(_numbers.size() >= _maxSize)
+		throw std::out_of_range("Span is full"); // ? what if runtime_error?
+	_numbers.push_back(number);
+}
+
+unsigned int Span::shortestSpan() const
+{
+	if (_numbers.size() < 2)
+		throw std::logic_error("Not enough numbers to compute a span");
+
+	std::vector<int> sorted = _numbers;
+	std::sort(sorted.begin(), sorted.end());
+
+	// unsigned int minSpan = static_cast<unsigned int>(-1); //trick to get max
+	// unsigned int minSpan = UINT_MAX;
+	// unsigned int minSpan = std::numeric_limits<unsigned int>::max(); // or use this
+	unsigned int minSpan = static_cast<unsigned int>(sorted[1] - sorted[0]); // initialize with first span
+	for (size_t i = 2; i < sorted.size(); ++i)
+	{
+		unsigned int span = static_cast<unsigned int>(sorted[i] - sorted[i - 1]);
+		if (span < minSpan)
+			minSpan = span;
+	}
+	
+	return minSpan;
+}
+
+unsigned int Span::longestSpan() const
+{
+	if (_numbers.size() < 2)
+		throw  std::logic_error("Not enough numbers to compute a span");
+		
+	int min = *std::min_element(_numbers.begin(), _numbers.end());
+	int max = *std::max_element(_numbers.begin(), _numbers.end());
+
+	return static_cast<unsigned int>(max - min);
+}
