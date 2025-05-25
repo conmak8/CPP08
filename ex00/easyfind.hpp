efb0183a29da05a92f03ea755dd67c947df991e3
Constantinos Makariou <cmakario@student.42heilbronn.de>
2025-05-25 19:39:28 +0200

    small changes

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,20 +6,20 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 13:54:44 by cmakario          #+#    #+#             */
-/*   Updated: 2025/05/03 14:41:04 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/05/25 19:28:41 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
-#define EASYFIND_HPP
+# define EASYFIND_HPP
 
-#include <algorithm>  // std::find
-#include <stdexcept>  // runtime_error
+# include <algorithm>  // std::find
+# include <stdexcept>  // runtime_error
 
 
 template <typename T>
 typename T::iterator easyfind(T& container, int value);
 
-#include "easyfind.tpp"
+# include "easyfind.tpp"
 
 #endif // !EASYFIND_HPP
